023a94bd64e366df058ad50af8fc962637239682
wasi881 <159992374+wasi881@users.noreply.github.com>
2024-05-14 09:48:28 +0500

    Add files via upload

---
+++ blbue/app.js
@@ -0,0 +1,8 @@
+function bluleon(){
+    var getimage = document.getElementById("imagee")
+    getimage.src = "light-bulb-png-bulb-png1247-12.png"
+}
+// function bluleon(){
+//     var getimage = document.getElementById("imagee")
+//     getimage.src = ""
+// }
